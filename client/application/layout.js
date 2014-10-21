8ac36b23c555901581a348c2d7ecbbd4a08602c6
knu2xs <knu2xs@gmail.com>
2014-10-21 14:33:17 -0400

    format layout.js

--- client/application/layout.js
+++ client/application/layout.js
@@ -1,3 +1,5 @@
-Meteor.typeahead('input#wbsSearch', function(){
-    return Wbs.find({modifier: false}).fetch().map(function(wbsItem){ return wbsItem.abbrev; });
+Meteor.typeahead('input#wbsSearch', function () {
+    return Wbs.find({modifier: false}).fetch().map(function (wbsItem) {
+        return wbsItem.abbrev;
+    });
 });
